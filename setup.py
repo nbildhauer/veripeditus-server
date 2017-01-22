a44d02d40bc0376f9ca3295d774e1c407c54c806
Dominik George <nik@naturalnet.de>
2017-01-23 00:54:12 +0100

    Remove another incorrect copyright mark.

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@
 
 # veripeditus-server - Server component for the Veripeditus game framework
 # Copyright (C) 2016  Dominik George <nik@naturalnet.de>
-# Copyright (c) 2016  mirabilos <m@mirbsd.org>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published
