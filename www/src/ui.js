be715f773187374fc06f9259dfa0c32a0a71340a
Dominik George <nik@naturalnet.de>
2017-01-22 23:24:29 +0100

    Add content to about dialog, advances #79.

--- www/src/ui.js
+++ www/src/ui.js
@@ -174,6 +174,10 @@ UIService = function () {
                     $("#dialog-player-login").show();
                     $("#dialog-player-logout").hide();
                 }
+
+                // Load COPYING file into element
+                $("#copying").load("COPYING");
+
                 // end of “player” view
             } else if (view == 'avatars') {
                 GameData.doRequestJSON('GET', '/api/v2/gameobject/' + GameData.current_player_id + '/available_images', avatars_handler);
